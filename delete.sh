7e230b2e3eb7f3eade00128a64bd8f90b06322cc
Getiria Onsongo <onsongoinc@gmail.com>
2015-05-11 14:54:53 -0500

    Re-organized folder

---
+++ delete.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+rm -rf *.pbs.*
+rm -rf *.gz
+rm -rf calibrate
+rm -rf DBI
+rm -rf getopt
+rm -rf MASS 
+rm -rf mysq*
+rm -rf optparse
+rm -rf plotrix
+rm -rf RMySQL
+rm -rf zoo
+rm -rf *.sql
+rm -rf test_cnv_*
+rm -rf *~
+rm -rf ordered_genes.txt
+rm -rf CopyNumberDeletion.R
